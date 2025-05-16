31a826df875c0257427d4dd736729bfd78a3d218
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-05-16 22:52:21 +0300

    cw_5 4

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("treker.urls", namespace="treker")),
+    path("treker", include("treker.urls", namespace="treker")),
     path("users/", include("users.urls", namespace="users")),
     path(
         "swagger/",
